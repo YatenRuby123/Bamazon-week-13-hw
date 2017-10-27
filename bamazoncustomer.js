7e671cacbd9f713aefed30e2d224cf6459a32294
Yatendra <rubyyataw2100@gmail.com>
2017-10-27 02:33:44 -0700

    password change

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -5,7 +5,7 @@ var Table = require('cli-table');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'MYdb2018!$', //password of db if any
+    password: '', //password of db if any
     database: 'Bamazon',
 });
 
